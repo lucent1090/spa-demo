1c3bb565362c25aab82704671f745ee8c6fd5dbf
lucent1090 <lucent1090@gmail.com>
2023-10-24 19:46:34 +0800

    perf: add lazy loading to router

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
+//import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./page/Layout";
 import Error from "./page/Error";
-import Home from "./page/Home";
-import Users from "./page/Users";
-import UserDetail from "./page/UserDetail";
+//import Home from "./page/Home";
+//import Users from "./page/Users";
+//import UserDetail from "./page/UserDetail";
 import { loader as homeLoader } from "./loader/home";
 import { loader as usersLoader } from "./loader/users";
 import { loader as userDetailLoader } from "./loader/userDetail";
@@ -17,17 +18,26 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        lazy: async () => {
+          const HomeComp = await import("./page/Home");
+          return { Component: HomeComp.default };
+        },
         loader: homeLoader,
       },
       {
         path: "users",
-        element: <Users />,
+        lazy: async () => {
+          const UserComp = await import("./page/Users");
+          return { Component: UserComp.default };
+        },
         loader: usersLoader,
       },
       {
         path: "users/:id",
-        element: <UserDetail />,
+        lazy: async () => {
+          const UserDetailComp = await import("./page/UserDetail");
+          return { Component: UserDetailComp.default };
+        },
         loader: userDetailLoader,
       },
     ],
